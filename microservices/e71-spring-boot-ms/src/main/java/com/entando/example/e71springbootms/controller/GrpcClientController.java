92b8921b4fa13d46ec0f3d2caa87efebb281023a
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2022-12-05 17:32:33 +0100

    upgrade eN7 template integration

--- microservices/e71-spring-boot-ms/src/main/java/com/entando/example/e71springbootms/controller/GrpcClientController.java
+++ microservices/e71-spring-boot-ms/src/main/java/com/entando/example/e71springbootms/controller/GrpcClientController.java
@@ -2,13 +2,18 @@ package com.entando.example.e71springbootms.controller;
 
 import com.agiletec.aps.system.services.group.Group;
 import com.agiletec.aps.system.services.group.IGroupManager;
+import com.entando.en7.MsApplicationContext;
 import java.util.List;
 //import javax.annotation.security.RolesAllowed;
 import org.entando.entando.ent.system.remotebean.annotation.ProxyAutowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GrpcClientController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(GrpcClientController.class);
     
     @ProxyAutowired
     private IGroupManager groupManager;
@@ -17,7 +22,14 @@ public class GrpcClientController {
     @GetMapping("/api/groups")
     // @RolesAllowed("role1")
     public MyGroupResponse getExample() {
-        return new MyGroupResponse(this.groupManager);
+        logger.error("INVOCATO SERVIZIO - {}", groupManager);
+        try {
+            return new MyGroupResponse(this.groupManager);
+        } catch (Exception e) {
+            logger.error("Errore", e);
+            throw e;
+        }
+        
     }
 
     public static class MyGroupResponse {
